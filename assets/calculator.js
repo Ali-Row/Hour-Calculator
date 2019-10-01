1d15085deadacf37fb7a371eca834106bbb465a1
Alistair Rowden <deadlyhardware@gmail.com>
2019-09-30 23:32:29 -0400

    Fixed total hours bug

--- assets/calculator.js
+++ assets/calculator.js
@@ -8,9 +8,9 @@ let tuesAndThurs = 4.25;
 // Hourly rate for Saturdays
 let saturdays = 4.75;
 // Total number of week days including Tuesdays and Thursdays
-let numWeekDays = 15;
+let numWeekDays = 14;
 // Total number of Saturdays
-let numSaturdays = 7;
+let numSaturdays = 6;
 // Hourly rate
 let perHour = 20;
 // Factoring in any missed class hours
@@ -19,19 +19,18 @@ let missedClassHours = 8.5;
     function workOutHours () {
         // Total week hours multiplied by number of week days
         let totalWeekHours = tuesAndThurs * numWeekDays;
-        console.log("This is the total hours worked on weekdays: " + totalWeekHours + " hours");
+        console.log("Total hours worked on weekdays: " + totalWeekHours + " hours");
         // Total Saturday hours multiplied by number of Saturdays
         let totalSatHours = saturdays * numSaturdays;
-        console.log("This is the total hours worked on Saturdays: " + totalSatHours + " hours");
-        // 
-        let totalHoursWorked = totalWeekHours + totalSatHours;
-        console.log("This is the total hours for every class: " + totalHoursWorked + " hours");
-
-        let missedPay = missedClassHours * perHour;
-
-        let totalAmountPaid = totalHoursWorked * perHour - missedPay;
-        console.log("This is the gross pay for every class worked: " + "$" + totalAmountPaid + ".00");
+        console.log("Total hours worked on Saturdays: " + totalSatHours + " hours");
+        // Work out total hours worked by combining weekdays with Saturdays hours
+        let totalHoursWorked = totalWeekHours + totalSatHours - missedClassHours;
+        console.log("Total hours including missed hours: " + totalHoursWorked + " hours");
+        // Work out the total gross pay including any missed classes
+        let totalAmountPaid = totalHoursWorked * perHour;
+        console.log("Gross pay for every class worked to date: " + "$" + totalAmountPaid + ".00");
     }
+
     // Running the function and passing in all of the variables
     workOutHours(tuesAndThurs, saturdays, numWeekDays, numSaturdays, perHour, missedClassHours);
 
