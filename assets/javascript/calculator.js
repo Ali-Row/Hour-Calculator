b2bd714b9ef8ee40168dffc7f5ae28551225c8c5
Alistair Rowden <deadlyhardware@gmail.com>
2019-10-01 01:55:51 -0400

    Got data showing up dynamically in HTML

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // regex used for days
 //`\b(\w*Tuesday\w*)\b`
 //`\b(\w*Thursday\w*)\b`
@@ -8,30 +10,61 @@ let tuesAndThurs = 4.25;
 // Hourly rate for Saturdays
 let saturdays = 4.75;
 // Total number of week days including Tuesdays and Thursdays
-let numWeekDays = 15;
+// let numWeekDays = 15;
+let numWeekDays = 0;
 // Total number of Saturdays
-let numSaturdays = 7;
+// let numSaturdays = 7;
+let numSaturdays = 0;
 // Hourly rate
 let perHour = 20;
 // Factoring in any missed class hours
 let missedClassHours = 8.5;
 
+    $('#submit').on('click', function () {
+        event.preventDefault();
+        numWeekDays = $('#weekdays').val();
+        numSaturdays = $('#saturdays').val();
+        // Running the function and passing in all of the variables
+        workOutHours(tuesAndThurs, saturdays, numWeekDays, numSaturdays, perHour, missedClassHours);
+    });
+
     function workOutHours () {
         // Total week hours multiplied by number of week days
         let totalWeekHours = tuesAndThurs * numWeekDays;
-        // console.log("Total hours worked on weekdays: " + totalWeekHours + " hours");
         // Total Saturday hours multiplied by number of Saturdays
         let totalSatHours = saturdays * numSaturdays;
-        // console.log("Total hours worked on Saturdays: " + totalSatHours + " hours");
         // Work out total hours worked by combining weekdays with Saturdays hours and subtracting any missed hours
         let totalHoursWorked = totalWeekHours + totalSatHours - missedClassHours;
-        console.log("Total hours including missed hours: " + totalHoursWorked + " hours");
+        $("#displayHours").html("<h5> Total hours including missed hours: " + "<h3>" + totalHoursWorked + " hours");
         // Work out the total gross pay including any missed classes
+
         let totalAmountPaid = totalHoursWorked * perHour;
-        console.log("Gross pay for every class worked to date: " + "$" + totalAmountPaid + ".00");
+        $("#displayPay").html("<h5> Gross pay for every class worked to date: " + "<h3>" + "$" + totalAmountPaid + ".00");
+
+            $("#main").html(`<hr class="my-4">
+
+            <div class="p-2">
+                <h5>Total hours including missed hours:<h3>${totalHoursWorked}</h3></h5>
+            </div>
+
+                <hr class="my-4">
+            
+            <div class="p-2">
+                <h5>Hourly rate:<h3>$${perHour}.00</h3></h5>
+            </div>
+            
+                <hr class="my-4">
+            
+            <div class="p-2">
+                <h5>Gross pay for every class worked to date:<h3>$${totalAmountPaid}.00</h3></h5>
+            </div>
+
+                <hr class="my-4">
+
+            <div class="p-2">
+            <h5>Total missed hours:<h3>${missedClassHours} hours</h3></h5>
+
+        </div>`)
     }
 
-    // Running the function and passing in all of the variables
-    workOutHours(tuesAndThurs, saturdays, numWeekDays, numSaturdays, perHour, missedClassHours);
-
-
+});
